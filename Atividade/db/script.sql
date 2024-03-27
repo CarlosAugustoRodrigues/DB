54dc88b471388907c8683c60ae096f5c82e61d61
CarlosAugustoRodrigues <carlos.augusto01503@hotmail.com>
2024-03-27 09:30:41 -0300

    att

--- Atividade/db/script.sql
+++ Atividade/db/script.sql
@@ -17,24 +17,3 @@ CREATE TABLE tarefas(
     status ENUM('Aberto', 'Em Andamento', 'Finalizado') NOT NULL,
     FOREIGN KEY (responsavel) REFERENCES usuarios(id)
 );
-
-INSERT INTO usuarios (nome, email, senha)
-VALUES 
-    ("Colaborador 1", "colaborador1@email.com", "987654321"),
-    ("Colaborador 2", "colaborador2@email.com", "123456789"),
-    ("Colaborador 3", "colaborador3@email.com", "123454321");
-
-
-INSERT INTO tarefas (descricao, data_limite, status, responsavel)
-VALUES 
-    ("Tarefa de teste 1", NOW() + INTERVAL 1 MONTH, "Aberto", 1),
-    ("Tarefa de teste 2", NOW() + INTERVAL 2 WEEK, "Em Andamento", 2),
-    ("Tarefa de teste 4", NOW() + INTERVAL 2 WEEK, "Em Andamento", 2),
-    ("Tarefa de teste 4", NOW() + INTERVAL 2 WEEK, "Em Andamento", 2),
-    ("Tarefa de teste 4", NOW() + INTERVAL 2 WEEK, "Em Andamento", 2),
-    ("Tarefa de teste 3", NOW() + INTERVAL 3 DAY, "Finalizado", 3);
-
-SELECT * FROM tarefas;
-SELECT * FROM usuarios;
-
-show tables;  
